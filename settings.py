9bc96fc2b7ae1c81a747d5893f9a1b0344b84fb2
Thiago Garcia <thiagogds14@gmail.com>
2011-03-23 16:25:24 -0300

    Instalado o south

--- settings.py
+++ settings.py
@@ -106,6 +106,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.admin',
+    'south',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'core',
